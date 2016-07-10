e7f920d52e14f5652097745bb87f8bb14328b8ca
muthuselvakumari <muthuselvakumari.m@gmail.com>
2016-07-10 11:36:34 +0530

    kinder.c

--- kinder.c
+++ kinder.c
@@ -3,8 +3,19 @@
 #include<conio.h>
 void main()
 {
-int val,div,pair,i,j;
+int val,di,pair,i,j;
 clrscr();
 printf("\n enter the value:");
 scanf("%d",&val);
-printf("\n enter the pair
+printf("\n enter the pair:");
+scanf("%d",&pair);
+for(i=0;i<2*val;i++)
+{
+di=2*val-i;
+if(pair!=di)
+{
+printf("The pair is%d for days %d is %d\n",pair,j++,pair);
+}
+}
+getch();
+}
